18a9b3e9ebbb149e7f443805a04351838b41d5ab
Hugo Vallada <valladahugo@gmail.com>
2022-09-21 07:20:18 -0300

    Adiciona um backend para o terraform

--- infra/esm.tf
+++ infra/esm.tf
@@ -1,5 +1,9 @@
+data "aws_dynamodb_table" "users" {
+  name = var.dynamodb_table
+}
+
 resource "aws_lambda_event_source_mapping" "esm" {
-  event_source_arn  = var.stream_arn
+  event_source_arn  = data.aws_dynamodb_table.users.stream_arn
   function_name     = var.lambda_arn
   starting_position = var.position
 }
--- infra/variable.tfvars
+++ infra/variable.tfvars
@@ -1,2 +1,2 @@
-lambda_arn = "arn:aws:lambda:sa-east-1:305889271744:function:dynamo-go"
-stream_arn = "arn:aws:dynamodb:sa-east-1:305889271744:table/Users/stream/2022-09-17T17:50:51.359"
+lambda_arn     = "arn:aws:lambda:sa-east-1:305889271744:function:dynamo-go"
+dynamodb_table = "Users"
